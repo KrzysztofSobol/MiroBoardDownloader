5b8d6bc1f09e47649e0b4baa5ce54aed3039b5b0
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2023-07-16 17:30:45 +0200

    Update MiroBoardDownloader_3.py

--- MiroBoardDownloader_3.py
+++ MiroBoardDownloader_3.py
@@ -1,4 +1,4 @@
-import pyautogui, time, subprocess, os, pyvips
+import pyautogui, time, subprocess
 from PIL import ImageGrab, Image
 
 path = f"d:/a little bit of programming/programming/studia/semestr III/1_PROJECTS/MiroBoardDownloader/screenshot/"
@@ -11,7 +11,6 @@ def screenshot(i):
     file_name = f"screenshot_{i}.png"
     ss = ImageGrab.grab(bbox=(57,58,1910,1020))  
     ss.save(f"{path}{file_name}")
-    return ss
 
 def GoDown(ssID):
     pyautogui.moveTo(1500, 1020)
@@ -31,8 +30,7 @@ def GoRight(ssID):
     time.sleep(0.5)
     pyautogui.mouseUp(button='left')
     time.sleep(1)
-    ss = screenshot(ssID)  
-    return ss
+    screenshot(ssID)
 
 def GoLeft(ssID):
     pyautogui.moveTo(57, 542)
@@ -42,8 +40,7 @@ def GoLeft(ssID):
     time.sleep(0.5)
     pyautogui.mouseUp(button='left') 
     time.sleep(1)
-    ss = screenshot(ssID)
-    return ss
+    screenshot(ssID)
 
 def border(ss, colors):
     screenshot_array = ss.load()
@@ -58,65 +55,67 @@ def border(ss, colors):
     else:
         return 0
 
-# scan
-width = 4
-w = 1
-height = 14
-h = 1
-ssID = 0
-screenshot(ssID)
-ssID=ssID+1
-while True:
+def scan_manual(width, height):
+    w = 1
+    h = 1
+    ssID = 0
+    screenshot(ssID)
+    ssID+=1
     while True:
-        if (w==width):
-            w = 1
+        while True:
+            if(w==width):
+                w = 1
+                break
+            if((h-1)%2)==0:
+                GoRight(ssID)
+            else:
+                GoLeft(ssID)
+            ssID=ssID+1
+            w = w + 1
+        if(h==height):
             break
-        if((h-1)%2)==0:
-         ss = GoRight(ssID)
-        else:
-         ss = GoLeft(ssID)
+        GoDown(ssID)
         ssID=ssID+1
-        w = w + 1
-    if (h==height):
-        break
-    GoDown(ssID)
-    ssID=ssID+1
-    h = h + 1
-print("Scan finished succesfully!")
+        h = h + 1
+    #print("Scan finished succesfully!")
+    # width=int(math.sqrt(width))
+    # print(f"height is {height}")
+    # print(f"width is {width}")
 
-#width=int(math.sqrt(width))
-print(f"height is {height}")
-print(f"width is {width}")
+def stirpes_merge(): # screenshot merge - stripes
+    ssID = 0
+    ss = Image.open(f"{path}screenshot_{ssID}.png")
+    ss_size = ss.size
+    BoardDownload = Image.new('RGB',(width*ss_size[0], ss_size[1]), (250,250,250))
+    for i in range(0,height): #height
+        for j in range(0,width): #width
+            if(i%2)==0:
+                ss = Image.open(f"{path}screenshot_{ssID}.png")
+                BoardDownload.paste(ss,(j*ss_size[0],0))
+            else:
+                ss = Image.open(f"{path}screenshot_{ssID}.png")
+                BoardDownload.paste(ss,((width-1-j)*ss_size[0],0))
+            ssID=ssID+1
+        BoardDownload.save(f"{path_board}BoardDownloadTest{i}_.png","PNG")
+    print("Stripes merge finished succesfully!")
 
-# screenshot merge - stripes
-ssID = 0
-ss = Image.open(f"{path}screenshot_{ssID}.png")
-ss_size = ss.size
-BoardDownload = Image.new('RGB',(width*ss_size[0], ss_size[1]), (250,250,250))
-for i in range(0    ,height): #height
-    for j in range(0,width): #width
-        if(i%2)==0:
-            ss = Image.open(f"{path}screenshot_{ssID}.png") 
-            BoardDownload.paste(ss,(j*ss_size[0],0)) 
-        else:
-            ss = Image.open(f"{path}screenshot_{ssID}.png") 
-            BoardDownload.paste(ss,((width-1-j)*ss_size[0],0))
-        ssID=ssID+1
-    BoardDownload.save(f"{path_board}BoardDownloadTest{i}_.png","PNG")
-print("Stripes merge finished succesfully!")
+def final_merge(height):
+    # Screenshot merge - final
+    input_images = [f"{path_board}BoardDownloadTest0_.png", f"{path_board}BoardDownloadTest1_.png"]
+    output_image = f"{path_final}temp0.png"
+    output_vimage = pyvips.Image.arrayjoin(input_images, across=1, shim=0)
+    output_vimage.write_to_file(output_image)
 
-# screenshot merge - final
-input_images = [f"{path_board}BoardDownloadTest0_.png", f"{path_board}BoardDownloadTest1_.png"]
-output_image = f"{path_final}temp0.png"
-subprocess.run(['vips', 'join'] + input_images + [output_image, 'vertical'])
+    for i in range(0, height - 2):
+        input_images = [f"{path_final}temp{i}.png", f"{path_board}BoardDownloadTest{i + 2}_.png"]
+        output_image = f"{path_final}temp{i + 1}.png"
+        output_vimage = pyvips.Image.arrayjoin(input_images, across=1, shim=0)
+        output_vimage.write_to_file(output_image)
+        file_path = f"{path_final}temp{i}.png"
+        os.remove(file_path)
+    print("Done!")
 
-for i in range(0,height-2):
-    input_images = [f"{path_final}temp{i}.png", f"{path_board}BoardDownloadTest{i+2}_.png"]
-    output_image = f"{path_final}temp{i+1}.png"
-    subprocess.run(['vips', 'join'] + input_images + [output_image, 'vertical'])
-    file_path = f"{path_final}temp{i}.png"
-    os.remove(file_path)
-print("Done!")
+final_merge(10)
 
 
 
