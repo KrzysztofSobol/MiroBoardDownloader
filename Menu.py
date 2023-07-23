5a7d5fea765d60d67876f134f201253dd86effac
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2023-07-23 18:43:25 +0200

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,4 +1,4 @@
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import tkinter as tk
 import customtkinter as ctk
 from customtkinter import CTkButton, CTkEntry, CTkLabel
@@ -6,7 +6,7 @@ from customtkinter import CTkButton, CTkEntry, CTkLabel
 
 
 # Button function for "Manual" button
-def button_clicked():
+def manual_button_clicked():
     global fields_visible
 
     if fields_visible:
@@ -29,13 +29,23 @@ def auto_button_clicked():
     select_folder()
     print("Auto button clicked!")
 
-# folder selection
+# Button function for "Start" button
+def start_button_clicked():
+    def update_text(count):
+        button_start.configure(text=f"Starting a scan in {count}")
+        if count > 0:
+            app.after(1000, update_text, count - 1)
+        else:
+            # countdown is finished, minimize the window
+            app.iconify()
+    # start the countdown
+    update_text(3)
+
+# Folder selection
 def select_folder():
     root = tk.Tk()
-    root.withdraw()  # Hide the main window
-
-    folder_path = filedialog.askdirectory(title="Select Folder")  # Open folder selection dialog
-
+    root.withdraw()
+    folder_path = filedialog.askdirectory(title="Select Folder")
     if folder_path:
         print("Selected folder path:", folder_path)
         # You can do further processing with the selected folder path here
@@ -56,12 +66,16 @@ y_label = CTkLabel(app, text="Height")
 y_entry = CTkEntry(app)
 
 # "Manual" button
-button_manual = CTkButton(app, text="Manual", command=button_clicked)
+button_manual = CTkButton(app, text="Manual", command=manual_button_clicked)
 button_manual.place(x=10, y=10)
 
 # "Auto" button
 button_auto = CTkButton(app, text="Auto", command=auto_button_clicked)
 button_auto.place(x=10, y=50)
+
+# "Start" button
+button_start = CTkButton(app, text="Start", command=start_button_clicked)
+button_start.place(x=570, y=440)
 
 # Flag to track visibility state
 fields_visible = False
