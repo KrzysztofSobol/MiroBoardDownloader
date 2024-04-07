369516fe069aed28fc9225127d9e8f0dd99dcecb
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2024-04-08 00:43:06 +0200

    Old updates
    Co-Authored-By: yb54y5byw5by5bKex <117276647+Kex4@users.noreply.github.com>

--- Menu.py
+++ Menu.py
@@ -1,9 +1,9 @@
-import tkinter as tk
 import customtkinter as ctk
 import MiroSystem as ms
-from customtkinter import CTkButton, CTkEntry, CTkLabel
-from tkinter import filedialog
+from customtkinter import *
+from threading import Thread
 import os
+
 
 # Load in the paths to the folders from a txt file
 def load_paths():
@@ -36,39 +36,36 @@ def write_to_txt_file():
         file.write(screenshotpath + '\n')
         file.write(finalpath + '\n')
 
+def TimeEstimate():
+    if(len(x_entry.get())!=0):
+        print(x_entry.get())
+
 # Button function for "Manual" button
 def manual_button_clicked():
     global fields_visible
 
     if fields_visible:
-        x_label.place_forget()
-        x_entry.place_forget()
-        y_label.place_forget()
-        y_entry.place_forget()
+        frame_manual.place_forget()
         fields_visible = False
-        button_auto.place(y=50)
 
     else:
-        x_label.place(x=10, y=50)
-        x_entry.place(x=55, y=50)
-        y_label.place(x=10, y=82)
-        y_entry.place(x=55, y=82)
+        frame_manual.place(x=0, y=48)
         fields_visible = True
-        button_auto.place(y=124)
-
-# Button function for "Auto" button
-def auto_button_clicked():
-    print("Auto button clicked!")
 
 # Button function for "Start" button
 def start_button_clicked():
+    def run_scan_manual(x, y, path, path_final):
+        ms.ScanManual(x, y, path)
+        ms.FinalMerge(x, y, path, path_final)
+        button_start.configure(text="Start")
     def update_text(count):
         button_start.configure(text=f"Starting a scan in {count}")
-        if count > 0:
+        if count > 0: # keep counting
             app.after(1000, update_text, count - 1)
         else:
             # countdown is finished
-            if ms.DoesOverlap("https://miro.com/"):
+            button_start.configure(text="In progress")
+            if ms.DoesOverlap("https://miro.com/"): # Is Miro overlapping
                 app.iconify()
             x_str = x_entry.get()
             y_str = y_entry.get()
@@ -78,9 +75,8 @@ def start_button_clicked():
             except ValueError:
                 print("ERROR: enter valid integers for x and y.")
                 return
-            print(path)
-            ms.ScanManual(x, y, path)
-            ms.FinalMerge(x, y, path, path_final)
+            scan_thread = Thread(target=run_scan_manual, args=(x, y, path, path_final))
+            scan_thread.start()
             return
     # start the countdown
     update_text(3)
@@ -95,105 +91,53 @@ def slider_changed(value):
 
 # Open settings window
 def open_settings_window():
-    # Settings functions
-    # Folder selection
-    def select_folder():
-        root = tk.Tk()
-        root.withdraw()
-        folder_path = filedialog.askdirectory(title="Select Folder")
-        if folder_path:
-            folder_path += "/"
-            return folder_path
-
-    # Button function for "accept" button in settings window
-    def accept_button_clicked():
-        write_to_txt_file()
-        load_paths()
-        settings_window.destroy()
-
-    # Button function for "cancle" button in settings window
-    def cancle_button_clicked():
-        settings_window.destroy()
-
-    # Button function for "cancle"
-    def findpath_button_clicked():
-        InputText_screenshotpath.insert(0,select_folder())
-    def findpath_button_clicked2():
-        InputText_finalpath.insert(0,select_folder())
-
-    # "Settings" app frame and settings
-    settings_window = ctk.CTkToplevel()
-    settings_window.geometry("410x480")
-    settings_window.title("Settings")
-    settings_window.transient(app)
     settings_window.grab_set()
-    settings_window.resizable(False, False)
-
-    frame_bottom_settings = ctk.CTkFrame(master=settings_window, width=410, height=55, border_width=-3, fg_color='#383838')
-    frame_bottom_settings.place(x=0, y=430)
-
-    # "Settings" app frame text input fields
-    global InputText_screenshotpath, InputText_finalpath
-    # text field for "screenshot" path
-    InputText_screenshotpath = CTkEntry(settings_window, width=340, height=40, placeholder_text=ms.get_line_from_file(data_path,1))
-    InputText_screenshotpath.place(x=10, y=40)
-    # text field for "final" path
-    InputText_finalpath = CTkEntry(settings_window, width=340, height=40, placeholder_text=ms.get_line_from_file(data_path,2))
-    InputText_finalpath.place(x=10, y=120)
-
-    # "Settings" app frame buttons
-    # "Accept" button
-    button_accept = CTkButton(frame_bottom_settings, text="Accept", command=accept_button_clicked)
-    button_accept.place(x=10, y=12)
-    # "Cancle" button
-    button_cancle = CTkButton(frame_bottom_settings, text="Cancle", command=cancle_button_clicked)
-    button_cancle.place(x=260, y=12)
-    # "Find Path" button
-    button_findpath = CTkButton(settings_window, width=50, height=40, text="set", command=findpath_button_clicked)
-    button_findpath.place(x=350, y=40)
-    button_findpath2 = CTkButton(settings_window, width=50, height=40, text="set", command=findpath_button_clicked2)
-    button_findpath2.place(x=350, y=120)
-
-    # "Settings" app frame text
-    # text field for screenshots
-    text_screenshot = CTkLabel(settings_window, text="Path to the folder that will store all of the single screenshots")
-    text_screenshot.place(x=10, y=10)
-    # text field for final merged screenshot
-    text_final = CTkLabel(settings_window, text="Path to the folder that will store a final scan")
-    text_final.place(x=10, y=90)
+    InputText_screenshotpath.configure(placeholder_text = ms.get_line_from_file(data_path, 1))
+    InputText_finalpath.configure(placeholder_text = ms.get_line_from_file(data_path, 2))
+    settings_window.place(x=280, y=1)
 
 # System settings
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("green")
 
-# App frame
+# ======================================= App frame =======================================
 app = ctk.CTk()
-app.geometry("720x480")
-app.title("Miro Board Downloader")
-app.iconbitmap('icon.ico')
+app.geometry("720x480") # Size of a main window
+app.title("Miro Board Downloader") # Name of a main window
+app.iconbitmap('icon.ico') # Icon of a main window
 app.resizable(False, False)
-
+# === Frames ===
 # Right_Side Frame
-frame_right_side = ctk.CTkFrame(master=app, width=440, height=665, border_width=-3, fg_color='#2e2e2e')
+frame_right_side = ctk.CTkFrame(master=app, width=440, height=665, border_width=3, fg_color='#2e2e2e', bg_color="#484444")
 frame_right_side.place(x=280, y=1)
 
 # Bottom frame
-frame_bottom = ctk.CTkFrame(master=app, width=720, height=55, border_width=-3, fg_color='#383838')
+frame_bottom = ctk.CTkFrame(master=app, width=720, height=55, border_width=3, fg_color='#383838', bg_color='#484444')
 frame_bottom.place(x=0, y=430)
 
+# X and Y entrys frame
+frame_manual = ctk.CTkFrame(master=app, width=210, height=61, border_width=-3, fg_color="#242424")
+
 # Create the X and Y input fields
-x_label = CTkLabel(app, text="Width")
-x_entry = CTkEntry(app)
-y_label = CTkLabel(app, text="Height")
-y_entry = CTkEntry(app)
+x_label = CTkLabel(frame_manual, text="Width")
+x_entry = CTkEntry(frame_manual)
+y_label = CTkLabel(frame_manual, text="Height")
+y_entry = CTkEntry(frame_manual)
+
+x_label.place(x=10, y=0)
+x_entry.place(x=55, y=0)
+y_label.place(x=10, y=32)
+y_entry.place(x=55, y=32)
+
+# "Progress" bar
+progress_bar = ctk.CTkProgressBar(frame_right_side, mode="determinate", width=400, height=10, bg_color="#2e2e2e")
+progress_bar.configure(fg_color="grey", progress_color="green")
+progress_bar.set(0)
+progress_bar.place(relx=0.5, rely=0.62, anchor="center")
 
 # "Manual" button
 button_manual = CTkButton(app, text="Manual", command=manual_button_clicked)
 button_manual.place(x=10, y=10)
-
-# "Auto" button
-button_auto = CTkButton(app, text="Auto", command=auto_button_clicked)
-button_auto.place(x=10, y=50)
 
 # "Start" button
 button_start = CTkButton(frame_bottom, text="Start", fg_color="green", hover_color="darkgreen", command=start_button_clicked)
@@ -203,12 +147,71 @@ button_start.place(x=570, y=12)
 button_settings = CTkButton(frame_bottom, text="Settings", command=settings_button_clicked)
 button_settings.place(x=10, y=12)
 
-# "Cooldown" Slider
-slider = ctk.CTkSlider(master=frame_right_side, from_=0, to=10, command=slider_changed)
-slider.place(x=200, y=100)
-
 # Flag to track visibility state
 fields_visible = False
+
+
+# ======================================= "Settings" app =======================================
+# Settings functions
+
+# Folder selection
+def select_folder():
+    folder_path = filedialog.askdirectory(title="Select Folder")
+    if folder_path:
+        folder_path += "/"
+        return folder_path
+
+# Button function for "accept" button in settings window
+def accept_button_clicked():
+    write_to_txt_file()
+    load_paths()
+    settings_window.grab_release()
+    settings_window.place_forget()
+
+# Button function for "cancle" button in settings window
+def cancle_button_clicked():
+    settings_window.grab_release()
+    settings_window.place_forget()
+
+# Button function for "cancle"
+def findpath_button_clicked():
+    InputText_screenshotpath.insert(0,select_folder())
+def findpath_button_clicked2():
+    InputText_finalpath.insert(0,select_folder())
+
+# "Settings" frame
+settings_window = ctk.CTkFrame(master=app, width=440, height=432, border_width=3, fg_color='#2e2e2e', bg_color="#484444")
+
+frame_bottom_settings = ctk.CTkFrame(master=settings_window, width=434, height=55, border_width=-3, fg_color='#383838')
+frame_bottom_settings.place(x=3, y=374)
+
+# "Settings" app frame text input fields
+# text field for "screenshot" path
+InputText_screenshotpath = CTkEntry(settings_window, width=369, height=40)
+InputText_screenshotpath.place(x=10, y=40)
+# text field for "final" path
+InputText_finalpath = CTkEntry(settings_window, width=369, height=40)
+InputText_finalpath.place(x=10, y=120)
+# "Settings" app frame buttons
+# "Accept" button
+button_accept = CTkButton(frame_bottom_settings, text="Accept", command=accept_button_clicked)
+button_accept.place(x=10, y=12)
+# "Cancle" button
+button_cancle = CTkButton(frame_bottom_settings, text="Cancle", command=cancle_button_clicked)
+button_cancle.place(x=287, y=12)
+# "Set" button
+button_findpath = CTkButton(settings_window, width=50, height=40, border_width=2, text="set", border_color="#50585c", command=findpath_button_clicked)
+button_findpath.place(x=379, y=40)
+button_findpath2 = CTkButton(settings_window, width=50, height=40, border_width=2, text="set", border_color="#50585c", command=findpath_button_clicked2)
+button_findpath2.place(x=379, y=120)
+
+# "Settings" app frame text
+# text field for screenshots
+text_screenshot = CTkLabel(settings_window, text="Path to the folder that will store all of the single screenshots")
+text_screenshot.place(x=10, y=10)
+# text field for final merged screenshot
+text_final = CTkLabel(settings_window, text="Path to the folder that will store a final scan")
+text_final.place(x=10, y=90)
 
 # Run app
 def on_app_start():
--- MiroSystem.py
+++ MiroSystem.py
@@ -1,11 +1,18 @@
 import pyautogui, time, os, pyvips
 from PIL import ImageGrab
 import pygetwindow as gw
+global progress
+global count
+count = 0
+
+def UptadeProgress(x, y):
+    progress = (x*y)/count
 
 def Screenshot(i, path):
     file_name = f"screenshot_{i}.png"
     ss = ImageGrab.grab(bbox=(57,58,1910,1020))  
     ss.save(f"{path}{file_name}")
+    count += 1
 
 def GoDown(ssID, path):
     Screenshot(ssID, path)
@@ -25,7 +32,7 @@ def GoRight(ssID, path):
     pyautogui.moveTo(57, 542)
     time.sleep(0.5)
     pyautogui.mouseUp(button='left')
-    time.sleep(1)
+    time.sleep(0.1)
 
 def GoLeft(ssID, path):
     Screenshot(ssID, path)
@@ -89,22 +96,25 @@ def DoesOverlap(URL):
 # function to scan an X by Y area on the board
 def ScanManual(width, height, path):
     ssID = 0
-    for h in range(0, height):
-        for w in range(1, width):
+    for h in range(height):
+        for w in range(width-1):
             if h % 2 == 0:
                 GoRight(ssID, path)
+                UptadeProgress(width, height)
             else:
                 GoLeft(ssID, path)
+                UptadeProgress(width, height)
             ssID += 1
-        if h < height:
+        if h < height - 1:
             GoDown(ssID, path)
+            UptadeProgress(width, height)
             ssID += 1
+    Screenshot(ssID, path)
+    UptadeProgress(width, height)
 
-# function to merge all the screenshots into one big image
-def FinalMerge(height, width, path, path_final):
+def FinalMerge(width, height, path, path_final):
     input_images = Upload_images_from_folder(path)
-
-    # Rearrange the images in a snake lookin pattern
+    # Rearrange the images in a snake-looking pattern
     rearranged_images = []
     for h in range(height):
         for w in range(width):
@@ -119,10 +129,6 @@ def FinalMerge(height, width, path, path_final):
     output_image = f"{path_final}FinalScan.png"
     output_vimage = pyvips.Image.arrayjoin(rearranged_images, across=width, shim=0, background=[0, 0, 0])
     output_vimage.write_to_file(output_image)
-
-
-
-
 
 
 
